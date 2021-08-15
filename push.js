2906ab912ce34684c92ecab4dc23c2021dce507e
sumatege <sumate.ge@gmail.com>
2021-08-15 23:32:31 +0700

    fix

--- push.js
+++ push.js
@@ -7,6 +7,14 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test@code.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {};
+let sub = { 
+    endpoint: 
+    "https://fcm.googleapis.com/fcm/send/f9J3G3Jcsu8:APA91bHsMdIERY0E51Hm_WrNUwRhoI26FPVzHWHXWaPcpZHqBqa5WEyoVeHchAsd6ap0xpisZBdiE1KGZb3VbwGbGxL0iyAenA6a6q1y5HXHQNow4Wo3kWSiBsIUmrW_5-dDyxYDgpa6",
+    expirationTime: null,
+    keys: { 
+        p256dh: "BJzF01NsdsDH4QWF7NtwGJ2C8WRQXue1tn8fsfUEFjQPt_GYcSnhrhCNBi82iJOFtRWfzaxgOtshWJKb62NhG4g", 
+        auth: "LaKgIMKno0bKVXWLF4JayQ" 
+    } 
+};
 
 push.sendNotification(sub, 'test message')
